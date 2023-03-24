433f28d5e0a1102771d79d6c4bbe9de9ae565b8f
MacMakus <119934279+MacMakus@users.noreply.github.com>
2023-03-24 16:18:40 +0700

    Add files via upload

---
+++ Model/SqlCommandModel/CmdInsert.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace PaperlessMergerWebForm.Model.SqlCommandModel
+{
+    public class CmdInsert
+    {
+        public void InsertInformation
+        (
+        StringBuilder sb, 
+        SqlConnection sqlCon,
+            string id_Number,
+            string title_Name_EN,
+            string first_Name_EN,
+            string last_Name_EN,
+            string middle_Name_EN,
+            string title_Name_TH,
+            string first_Name_TH,
+            string last_Name_TH,
+            string middle_Name_TH,
+            string id_passport,
+            string nationality,
+            DateTime dateOfBirth,
+            string sex,
+            string address1,
+            string district,
+            string county,
+            int postCode,
+            string country,
+            string email,
+            string telephoneNumber,
+            int status,
+            int clubId
+        )
+        {
+            SqlCommand sqlCmd = new SqlCommand(sb.ToString(), sqlCon);
+
+            sqlCmd.Parameters.AddWithValue("@ThaiId", id_Number);
+            sqlCmd.Parameters.AddWithValue("@TitleNameEN", title_Name_EN);
+            sqlCmd.Parameters.AddWithValue("@FirstNameEN", first_Name_EN);
+            sqlCmd.Parameters.AddWithValue("@LastNameEN", last_Name_EN);
+            sqlCmd.Parameters.AddWithValue("@MiddleNameEN", middle_Name_EN);
+            sqlCmd.Parameters.AddWithValue("@TitleNameTH", title_Name_TH);
+            sqlCmd.Parameters.AddWithValue("@FirstNameTH", first_Name_TH);
+            sqlCmd.Parameters.AddWithValue("@LastNameTH", last_Name_TH);
+            sqlCmd.Parameters.AddWithValue("@MiddleNameTH", middle_Name_TH);
+            sqlCmd.Parameters.AddWithValue("@PassportId", id_passport);
+            sqlCmd.Parameters.AddWithValue("@Nationality", nationality);
+            sqlCmd.Parameters.AddWithValue("@DateOfBirth", dateOfBirth);
+            sqlCmd.Parameters.AddWithValue("@Sex", sex);
+            sqlCmd.Parameters.AddWithValue("@Address1", address1);
+            sqlCmd.Parameters.AddWithValue("@District", district);
+            sqlCmd.Parameters.AddWithValue("@County", county);
+            sqlCmd.Parameters.AddWithValue("@PostCode", postCode);
+            sqlCmd.Parameters.AddWithValue("@Country", country);
+            sqlCmd.Parameters.AddWithValue("@Email", email);
+            sqlCmd.Parameters.AddWithValue("@TelephoneNumber", telephoneNumber);
+            sqlCmd.Parameters.AddWithValue("@Status", status);
+            sqlCmd.Parameters.AddWithValue("@ClubId", clubId);
+
+            sqlCmd.ExecuteNonQuery();
+        }
+    }
+}
---
+++ Model/StringBuilderModel/SbInsert.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace PaperlessMergerWebForm.Model.StringBuilderModel
+{
+    public class SbInsert
+    {
+        public StringBuilder InsertInformation()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(" INSERT INTO ");
+            sb.AppendLine(" ClientInformation ");
+            sb.AppendLine(" ( ");
+            sb.AppendLine(" ThaiId, ");
+            sb.AppendLine(" TitleNameEN, ");
+            sb.AppendLine(" FirstNameEN, ");
+            sb.AppendLine(" LastNameEN, ");
+            sb.AppendLine(" MiddleNameEN, ");
+            sb.AppendLine(" TitleNameTH, ");
+            sb.AppendLine(" FirstNameTH, ");
+            sb.AppendLine(" LastNameTH, ");
+            sb.AppendLine(" MiddleNameTH, ");
+            sb.AppendLine(" PassportId, ");
+            sb.AppendLine(" Nationality, ");
+            sb.AppendLine(" DateOfBirth, ");
+            sb.AppendLine(" Sex, ");
+            sb.AppendLine(" Address1, ");
+            sb.AppendLine(" District, ");
+            sb.AppendLine(" County, ");
+            sb.AppendLine(" PostCode, ");
+            sb.AppendLine(" Country, ");
+            sb.AppendLine(" Email, ");
+            sb.AppendLine(" TelephoneNumber, ");
+            sb.AppendLine(" Status, ");
+            sb.AppendLine(" ClubId ");
+            sb.AppendLine(" ) ");
+            sb.AppendLine(" VALUES ");
+            sb.AppendLine(" ( ");
+            sb.AppendLine(" @ThaiId, ");
+            sb.AppendLine(" @TitleNameEN, ");
+            sb.AppendLine(" @FirstNameEN, ");
+            sb.AppendLine(" @LastNameEN, ");
+            sb.AppendLine(" @MiddleNameEN, ");
+            sb.AppendLine(" @TitleNameTH, ");
+            sb.AppendLine(" @FirstNameTH, ");
+            sb.AppendLine(" @LastNameTH, ");
+            sb.AppendLine(" @MiddleNameTH, ");
+            sb.AppendLine(" @PassportId, ");
+            sb.AppendLine(" @Nationality, ");
+            sb.AppendLine(" @DateOfBirth, ");
+            sb.AppendLine(" @Sex, ");
+            sb.AppendLine(" @Address1, ");
+            sb.AppendLine(" @District, ");
+            sb.AppendLine(" @County, ");
+            sb.AppendLine(" @PostCode, ");
+            sb.AppendLine(" @Country, ");
+            sb.AppendLine(" @Email, ");
+            sb.AppendLine(" @TelephoneNumber, ");
+            sb.AppendLine(" @Status, ");
+            sb.AppendLine(" @ClubId ");
+            sb.AppendLine(" ) ");
+
+            return sb;
+        }
+    }
+}
